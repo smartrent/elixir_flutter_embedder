114f8daf1057cefd6ef674888bb215ddc510b6db
Connor Rigby <connorr@hey.com>
2020-09-09 11:49:10 -0700

    Initial pass at drm embedder

---
+++ nerves_example/mix.exs
@@ -0,0 +1,68 @@
+defmodule NervesExample.MixProject do
+  use Mix.Project
+
+  @app :nerves_example
+  @version "0.1.0"
+  @all_targets [:bbb]
+
+  def project do
+    [
+      app: @app,
+      version: @version,
+      elixir: "~> 1.9",
+      archives: [nerves_bootstrap: "~> 1.8"],
+      start_permanent: Mix.env() == :prod,
+      build_embedded: true,
+      aliases: [loadconfig: [&bootstrap/1]],
+      deps: deps(),
+      releases: [{@app, release()}],
+      preferred_cli_target: [run: :host, test: :host],
+      compilers: Mix.compilers() ++ [:flutter_embedder]
+    ]
+  end
+
+  # Starting nerves_bootstrap adds the required aliases to Mix.Project.config()
+  # Aliases are only added if MIX_TARGET is set.
+  def bootstrap(args) do
+    Application.start(:nerves_bootstrap)
+    Mix.Task.run("loadconfig", args)
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      mod: {NervesExample.Application, []},
+      extra_applications: [:logger, :runtime_tools]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # Dependencies for all targets
+      {:nerves, "~> 1.6.0", runtime: false},
+      {:shoehorn, "~> 0.6"},
+      {:ring_logger, "~> 0.6"},
+      {:toolshed, "~> 0.2"},
+
+      # Dependencies for all targets except :host
+      {:nerves_runtime, "~> 0.6", targets: @all_targets},
+      {:nerves_pack, "~> 0.2", targets: @all_targets},
+
+      # Dependencies for specific targets
+      {:nerves_system_bbb_sgx,
+       path: "/home/connor/workspace/flutter/nerves_system_bbb_sgx", runtime: false, targets: :bbb},
+      {:flutter_embedder, path: "../"}
+    ]
+  end
+
+  def release do
+    [
+      overwrite: true,
+      cookie: "#{@app}_cookie",
+      include_erts: &Nerves.Release.erts/0,
+      steps: [&Nerves.Release.init/1, :assemble],
+      strip_beams: Mix.env() == :prod
+    ]
+  end
+end
