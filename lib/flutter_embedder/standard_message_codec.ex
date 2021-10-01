c6f3adea330b42c7fb533d729ef321550f5c5e80
Connor Rigby <connorr@hey.com>
2021-10-01 08:05:28 -0600

    refactor standard_messag_coded
    This is not super idiomatic, but it looks very similar to the dart
    implementation

--- lib/flutter_embedder/standard_message_codec.ex
+++ lib/flutter_embedder/standard_message_codec.ex
@@ -37,35 +37,169 @@ defmodule FlutterEmbedder.StandardMessageCodec do
                   is_list(value) or
                   is_nil(value)
 
-  @spec encode_value(value()) :: binary()
-  def encode_value(nil), do: <<@kStdNull>>
-  def encode_value(true), do: <<@kStdTrue>>
-  def encode_value(false), do: <<@kStdFalse>>
+  defmodule Buffer do
+    require Logger
 
-  def encode_value(int32) when is_integer(int32) and abs(int32) <= 0x7FFFFFFF,
+    def new() do
+      <<>>
+    end
+
+    def put_uint8(buffer, value) do
+      buffer <> <<value::unsigned-8>>
+    end
+
+    def put_uint16(buffer, value) do
+      buffer <> <<value::unsigned-native-16>>
+    end
+
+    def put_uint32(buffer, value) do
+      buffer <> <<value::unsigned-native-32>>
+    end
+
+    def put_int32(buffer, value) do
+      buffer <> <<value::signed-native-32>>
+    end
+
+    def put_int64(buffer, value) do
+      buffer <> <<value::signed-native-64>>
+    end
+
+    def put_float64(buffer, value) do
+      buffer = align_to(buffer, 8)
+      buffer <> <<value::signed-native-float-64>>
+    end
+
+    def put_uint8_list(buffer, list) do
+      buffer <> list
+    end
+
+    defp mod(x, y) when x > 0, do: rem(x, y)
+    defp mod(x, y) when x < 0, do: rem(x, y) + y
+    defp mod(0, _y), do: 0
+
+    def align_to(buffer, alignment) do
+      mod = mod(byte_size(buffer) + 1, alignment)
+      Logger.info(%{mod: mod, alignment: alignment, size: byte_size(buffer)})
+
+      if mod != 0 do
+        pad = alignment - mod
+        Logger.info(%{pad: pad})
+        buffer <> <<0::size(pad)-unit(8)>>
+      else
+        buffer
+      end
+    end
+
+    def write_size(buffer, size) when size < 254 do
+      put_uint8(buffer, size)
+    end
+
+    def write_size(buffer, size) when size <= 0xFFFF do
+      buffer
+      |> put_uint8(254)
+      |> put_uint16(size)
+    end
+
+    def write_size(buffer, size) when size <= 0xFFFFFFFF do
+      buffer
+      |> put_uint8(255)
+      |> put_uint32(size)
+    end
+  end
+
+  require Logger
+  @spec encode_value(value()) :: binary()
+  def encode_value(value) do
+    r = write_value(value, Buffer.new())
+    Logger.debug(%{value => r})
+    r
+  end
+
+  def write_value(value, buffer) do
+    cond do
+      is_nil(value) ->
+        Buffer.put_uint8(buffer, @kStdNull)
+
+      value == true ->
+        Buffer.put_uint8(buffer, @kStdTrue)
+
+      value == false ->
+        Buffer.put_uint8(buffer, @kStdFalse)
+
+      is_float(value) ->
+        buffer
+        |> Buffer.put_uint8(@kStdFloat64)
+        |> Buffer.put_float64(value)
+
+      is_integer(value) and abs(value) <= 0x7FFFFFFF ->
+        buffer
+        |> Buffer.put_uint8(@kStdInt32)
+        |> Buffer.put_int32(value)
+
+      is_integer(value) and abs(value) <= 0x7FFFFFFFFFFFFFFF ->
+        buffer
+        |> Buffer.put_uint8(@kStdInt32)
+        |> Buffer.put_int64(value)
+
+      is_binary(value) ->
+        buffer
+        |> Buffer.put_uint8(@kStdString)
+        |> Buffer.write_size(byte_size(value))
+        |> Buffer.put_uint8_list(value)
+
+      is_list(value) ->
+        buffer
+        |> Buffer.put_uint8(@kStdList)
+        |> Buffer.write_size(length(value))
+        |> (fn buffer ->
+              Enum.reduce(value, buffer, &write_value/2)
+            end).()
+
+      is_map(value) ->
+        buffer
+        |> Buffer.put_uint8(@kStdMap)
+        |> Buffer.write_size(map_size(value))
+        |> (fn buffer ->
+              Enum.reduce(value, buffer, fn {key, value}, acc ->
+                acc = write_value(to_string(key), acc)
+                write_value(value, acc)
+              end)
+            end).()
+
+      true ->
+        Logger.error("Encoder failed: #{inspect(value)}")
+        raise ArgumentError, value: value
+    end
+  end
+
+  def encode_term(nil), do: <<@kStdNull>>
+  def encode_term(true), do: <<@kStdTrue>>
+  def encode_term(false), do: <<@kStdFalse>>
+
+  def encode_term(int32) when is_integer(int32) and abs(int32) <= 0x7FFFFFFF,
     do: <<@kStdInt32, int32::signed-native-32>>
 
-  def encode_value(int64) when is_integer(int64) and abs(int64) <= 0x7FFFFFFFFFFFFFFF,
+  def encode_term(int64) when is_integer(int64) and abs(int64) <= 0x7FFFFFFFFFFFFFFF,
     do: <<@kStdInt64, int64::signed-native-64>>
 
-  def encode_value(float64) when is_float(float64),
+  def encode_term(float64) when is_float(float64),
     do: <<@kStdFloat64, 0::6*8, float64::signed-native-float-64>>
 
-  def encode_value(string) when is_binary(string) and byte_size(string) < 254 do
+  def encode_term(string) when is_binary(string) and byte_size(string) < 254 do
     <<@kStdString, byte_size(string)::8, string::binary>>
   end
 
-  def encode_value(string) when is_binary(string) and byte_size(string) < 0xFFFF do
+  def encode_term(string) when is_binary(string) and byte_size(string) < 0xFFFF do
     <<@kStdString, 254, byte_size(string)::native-16, string::binary>>
   end
 
   # TODO encode @kStdUInt8Array, @kStdInt32Array, @kStdInt64Array, @kStdFloat64Array
-  def encode_value(value) when is_list(value) do
+  def encode_term(value) when is_list(value) do
     acc = <<@kStdList, length(value)::8>>
 
     Enum.reduce(value, acc, fn
       value, acc when is_valid_dart_value(value) ->
-        acc <> encode_value(value)
+        acc <> encode_term(value)
 
       _invalid, _acc ->
         raise ArgumentError
@@ -73,13 +207,13 @@ defmodule FlutterEmbedder.StandardMessageCodec do
   end
 
   # i don't think Dart actually allows for maps as return values via PlatformChannel
-  def encode_value(%{} = map) do
+  def encode_term(%{} = map) do
     acc = <<@kStdMap, map_size(map)::8>>
 
     Enum.reduce(map, acc, fn
       # Dart only allows string keys
       {key, value}, acc when is_binary(key) and is_valid_dart_value(value) ->
-        acc <> encode_value(key) <> encode_value(value)
+        acc <> encode_term(key) <> encode_term(value)
 
       {_key, _value}, _acc ->
         raise ArgumentError
